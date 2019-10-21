f1328c88dd0446bf377070f11c7e8346136a7dc6
amiralles <amiralles.net@gmail.com>
2019-10-21 10:29:49 -0300

    fix some rules.

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Testing Script
+./hw1 ccx/complex.cci > out/complex.cci.out
+./hw1 ccx/complex.ccx > out/complex.ccx.out
+./hw1 ccx/date.cci > out/date.cci.out
+./hw1 ccx/date.ccx > out/date.ccx.out
+./hw1 ccx/hello_world.ccx > out/hello_worl.ccx.out
+./hw1 ccx/list.cci > out/list.cci.out
+./hw1 ccx/list.ccx > out/list.ccx.out
+./hw1 ccx/natural.cci > out/natural.cci.out
+./hw1 ccx/natural.ccx > out/natural.ccx.out
+./hw1 ccx/trie.cci > out/trie.cci.out
+./hw1 ccx/trie.ccx > out/trie.ccx.out
+./hw1 ccx/widget.ccx > out/widget.ccx.out
+./hw1 ccx/widget.cci > out/widget.cci.out
+
+# TODO:
+# diff out ccx_out
